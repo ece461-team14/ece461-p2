240068de51874107cc71ec4bbf1c20329d698fc4
Jackson Fair <jaxsofair@gmail.com>
2024-10-27 00:11:26 -0400

    Fix broken Deployment
    Testing yet again

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo 'compile typescript: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
+npm run build >> /home/ubuntu/ece461-p2/logs/deploy.log
 echo 'run application_start.sh: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
 echo 'pm2 restart TPR' >> /home/ubuntu/ece461-p2/logs/deploy.log
 pm2 restart TPR >> /home/ubuntu/ece461-p2/logs/deploy.log
