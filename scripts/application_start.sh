c7c2301b71a80bc15d4712dd113e48214155cf9c
Jackson Fair <jaxsofair@gmail.com>
2024-10-27 00:18:42 -0400

    Fix App Reload
    Fixing the source that the PM2 process is pulling from post-deployment

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,7 @@
 echo 'compile typescript: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
 npm run build >> /home/ubuntu/ece461-p2/logs/deploy.log
 echo 'run application_start.sh: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
+echo 'pm2 stop TPR' >> /home/ubuntu/ece461-p2/logs/deploy.log
+pm2 stop TPR >> /home/ubuntu/ece461-p2/logs/deploy.log
 echo 'pm2 restart TPR' >> /home/ubuntu/ece461-p2/logs/deploy.log
 pm2 restart TPR >> /home/ubuntu/ece461-p2/logs/deploy.log
