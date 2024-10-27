ef6afcd5287c3850eb91718cacfa4620770530b4
Ubuntu <ubuntu@ip-172-31-92-49.ec2.internal>
2024-10-27 03:34:15 +0000

    Testing Deployment

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
 
-echo 'cd /home/ec2-user/myrepo' >> /home/ubuntu/ece461-p2/logs/deploy.log
-cd /home/ec2-user/myrepo >> /home/ubuntu/ece461-p2/logs/deploy.log
+echo 'cd /home/ubuntu/ece461-p2' >> /home/ubuntu/ece461-p2/logs/deploy.log
+cd /home/ubuntu/ece461-p2 >> /home/ubuntu/ece461-p2/logs/deploy.log
 
 echo 'npm install' >> /home/ubuntu/ece461-p2/logs/deploy.log 
 npm install >> /home/ubuntu/ece461-p2/logs/deploy.log
