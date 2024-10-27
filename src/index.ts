3e0e42d2575e6b9ccfed004470954fb4b6ceef6f
Jackson Fair <jaxsofair@gmail.com>
2024-10-27 00:07:24 -0400

    Test Deployment

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  res.send("Balls.");
+  res.send("Hello AWS.");
 });
 
 app.listen(port, () => {
