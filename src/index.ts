81ba7369b69387d827ff978aae290ad8918fa487
Jackson Fair <jaxsofair@gmail.com>
2024-10-29 14:29:53 -0400

    Improved Comments
    Lets not be _too_ verbose now.

--- src/index.ts
+++ src/index.ts
@@ -8,54 +8,47 @@ app.get("/", (req, res) => {
   res.send("Hello World.");
 });
 
-// TODO: implement this endpoint
-// /packages endpoint
+// TODO: implement /packages endpoint
 // (Get the packages from the registry.)
 app.post("/packages", (req, res) => {});
 
-// TODO: implement this endpoint
-// /reset endpoint
+// TODO: implement /reset endpoint
 // (Reset the registry.)
 app.delete("/reset", (req, res) => {});
 
-// TODO: implement this endpoint
-// /package/{id} get endpoint (interacts with package)
+// TODO: implement /package/{id} get endpoint
 // (Interact with the package with this ID.)
 app.get("/package/:id", (req, res) => {});
 
-// TODO: implement this endpoint
-// /package/{id} post endpoint (updates package)
-// (Update this content of the package.)
+// TODO: implement /package/{id} post endpoint
+// (Update the content of this package.)
 app.post("/package/:id", (req, res) => {});
 
-// TODO: implement this endpoint
-// /package endpoint
+// TODO: implement /package endpoint
 // (Upload or ingest a new package.)
 app.post("/package", (req, res) => {});
 
-// TODO: implement this endpoint
-// /package/{id}/rate endpoint
+// TODO: implement /package/{id}/rate
 // (Get ratings for this package.)
 app.get("/package/:id/rate", (req, res) => {});
 
-// TODO: implement this endpoint
+// TODO: implement /package/{id}/cost
 // /package/{id}/cost endpoint
 // (Get the cost of this package.)
 app.get("/package/:id/cost", (req, res) => {});
 
-// TODO: implement this endpoint
-// /authenticate endpoint
+// TODO: implement /authenticate
+//  endpoint
 // (Create an access token.)
 // (NON-BASELINE)
 app.put("/authenticate", (req, res) => {});
 
-// TODO: implement this endpoint
-// /package/byRegEx endpoint
+// TODO: implement /package/byRegEx
 // (Get any packages fitting the regular expression.)
 app.post("/package/byRegEx", (req, res) => {});
 
 // FIXME: take out the tracks we aren't doing
-// /tracks endpoint
+// (Get the list of tracks the team is implementing.)
 app.get("/tracks", (req, res) => {
   try {
     const tracks = [
