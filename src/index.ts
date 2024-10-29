357f60cbe26aafe81c06c519b7f4d77fc649addb
James Murrer <jamesmurrer@pal-nat186-40-230.itap.purdue.edu>
2024-10-29 14:33:09 -0400

    added button and text input

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Merci (Jackson's Girlfriend).");
+  res.send(`
+    <html>
+      <body>
+        <h1>Big moves from hello world</h1>
+        <form action="/search" method="get">
+          <label for="query">link or other entered here:</label>
+          <input type="text" id="query" name="query" required>
+          <button type="submit">Search</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.get("/search", (req, res) => {
+  const query = req.query.query as string;
+  res.send(`here's the search: ${query}`);
 });
 
 app.listen(port, () => {
