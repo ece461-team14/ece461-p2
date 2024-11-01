3f4e2ccf03e2202c4b4e21c502196556b69f0fec
Jackson Fair <jaxsofair@gmail.com>
2024-11-01 05:57:31 -0400

    More package.lock fixes
    Got rid of the back-end package.lock in favor of a central one that can handle all dependencies and a very rudimentary one so that react-scripts plays nice.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -23,38 +23,10 @@ function App() {
         <p>Press the button to get a message from the back-end:</p>
         <button onClick={fetchMessage}>Fetch Message</button>
         {message && <p>Back-end Response: {message}</p>}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
   //--------------------------------
-
-  // Auto-generated code from create-react-app
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-}
+  }
 
 export default App;
