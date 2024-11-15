d510a5319c395b8a4d4d602c48b38e2a4f0cbb7d
Jackson Fair <jaxsofair@gmail.com>
2024-11-15 15:23:08 -0500

    Removed Deprecated Deployment Scripts
    Removed after_install.sh and application_start.sh as they were both being used by the previously used AWS deployment commands and no longer need to be worried about post Jaxdeploy invention

--- scripts/application_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'run application_start.sh: ' >> /home/ubuntu/ece461-p2/logs/deploy.log
-echo 'pm2 restart TPR' >> /home/ubuntu/ece461-p2/logs/deploy.log
-pm2 restart TPR >> /home/ubuntu/ece461-p2/logs/deploy.log
