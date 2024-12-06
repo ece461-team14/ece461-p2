d90049a8ef275b85f569e4cd8d40cbaacdfb0cd2
Jackson <jaxsofair@gmail.com>
2024-12-05 23:29:18 -0500

    Update getTracks.ts
    Semantic changes to return object for autograder

--- back/src/endpoints/getTracks.ts
+++ back/src/endpoints/getTracks.ts
@@ -1,6 +1,6 @@
 export const getTracks = (req, res) => {
   try {
-    const plannedTracks = ["Access Control Track"];
+    const plannedTracks = ["Access control track"];
     res.json({ plannedTracks });
   } catch (err) {
     res.status(500).json({
