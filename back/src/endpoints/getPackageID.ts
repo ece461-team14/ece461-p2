25e31b7530c1d486fb56eeee72f9d477cd1808e1
bmac2003 <btmclaughlin03@gmail.com>
2024-12-08 18:19:37 -0500

    JSProgram functionality for package download

--- back/src/endpoints/getPackageID.ts
+++ back/src/endpoints/getPackageID.ts
@@ -2,6 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 import * as fs from "fs";
 import { idExists, getObjFromId } from "../utils/idReg.js";
 import { getPackageFromID } from "../utils/s3Utils.js";
+import { executeJsOnZip } from "../utils/runJSProgram.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
@@ -31,53 +32,65 @@ export const getPackageID = async (req, res) => {
     }
 
     // Verify the JWT token
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-      if (err) {
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res
+        .status(403)
+        .send(
+          "Authentication failed due to invalid or missing AuthenticationToken."
+        );
+    }
+
+    const username = decoded.name;
+
+    // Proceed with the request handling (retrieving package)
+    const bucketName = process.env.S3_BUCKET; // Get bucket name from environment
+    if (!bucketName) {
+      return res.status(500).send("S3 bucket name is not set.");
+    }
+
+    const regCache = JSON.parse(fs.readFileSync("registry.json", "utf-8"));
+
+    // Check package existence using package ID
+    try {
+      // check if package ID exists in regCache
+      if (!idExists(regCache, packageId)) {
+        return res.status(404).send("Package does not exist.");
+      }
+    } catch (err) {
+      console.error("Error checking package existence:", err);
+      return res.status(500).send("Error retrieving package metadata.");
+    }
+
+    // get object with given ID
+    const metadata = getObjFromId(regCache, packageId);
+
+    // Get the package content
+    const packageResponse = await getPackageFromID(bucketName, packageId);
+    const content = packageResponse.toString("base64");
+
+    if (metadata.JSProgram) {
+      const programResponse = await executeJsOnZip(content, metadata.JSProgram, metadata, username);
+      if (programResponse === 1) {
         return res
-          .status(403)
-          .send(
-            "Authentication failed due to invalid or missing AuthenticationToken."
-          );
+          .status(406)
+          .send("Package has failed sensitive module check.");
       }
+    }
 
-      // Proceed with the request handling (retrieving package)
-      const bucketName = process.env.S3_BUCKET; // Get bucket name from environment
-      if (!bucketName) {
-        return res.status(500).send("S3 bucket name is not set.");
-      }
-
-      const regCache = JSON.parse(fs.readFileSync("registry.json", "utf-8"));
-
-      // Check package existence using package ID
-      try {
-        // check if package ID exists in regCache
-        if (!idExists(regCache, packageId)) {
-          return res.status(404).send("Package does not exist.");
-        }
-      } catch (err) {
-        console.error("Error checking package existence:", err);
-        return res.status(500).send("Error retrieving package metadata.");
-      }
-
-      // get object with given ID
-      const metadata = getObjFromId(regCache, packageId);
-
-      // Get the package content
-      const packageResponse = await getPackageFromID(bucketName, packageId);
-      const content = packageResponse.toString("base64");
-
-      // Send response with metadata and content
-      res.status(200).json({
-        metadata: {
-          Name: metadata.Name,
-          Version: metadata.Version,
-          ID: metadata.ID,
-        },
-        data: {
-          // send content in base64 format
-          Content: content,
-        },
-      });
+    // Send response with metadata and content
+    res.status(200).json({
+      metadata: {
+        Name: metadata.Name,
+        Version: metadata.Version,
+        ID: metadata.ID,
+      },
+      data: {
+        // send content in base64 format
+        Content: content,
+      },
     });
   } catch (error) {
     console.error("Error handling /package/:id request:", error);
